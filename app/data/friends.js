1a01683275830d33fc6d2aa45a23f6e91da998e6
Christian Maldonado <chris.maldonado99@gmail.com>
2020-05-07 14:20:37 -0700

    added a couple changed to html side

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 // Data structure that will store the friends data
 var friendsArray = [
 	{
-		name: 'Scooter',
+		name: 'Bro',
 		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
 		scores: [
 			'1',
@@ -17,7 +17,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Fluffy',
+		name: 'Fluffles',
 		photo: 'https://i.ytimg.com/vi/mRf3-JkwqfU/hqdefault.jpg',
 		scores: [
 			'2',
@@ -33,7 +33,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Spencer',
+		name: 'Target Dog',
 		photo: 'http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg',
 		scores: [
 			'4',
@@ -49,7 +49,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Zackariah',
+		name: 'big bird',
 		photo: 'https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg',
 		scores: [
 			'5',
@@ -65,7 +65,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Cloe',
+		name: 'Micky Mouse',
 		photo: 'http://4.bp.blogspot.com/-HTvSYzA-pO4/UgQb4Zh_u0I/AAAAAAAAEuI/XwhtogT_1tA/s1600/3+cute2.jpg',
 		scores: [
 			'1',
@@ -81,7 +81,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Mrs. Piggy',
+		name: 'cookie monster',
 		photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg',
 		scores: [
 			'1',
@@ -97,7 +97,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Beyonce Knowles',
+		name: 'selena',
 		photo: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
 		scores: [
 			'5',
@@ -113,7 +113,7 @@ var friendsArray = [
 		]
 	},
 	{
-		name: 'Adele Adkins',
+		name: 'chirstian',
 		photo: 'https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpg',
 		scores: [
 			'5',
